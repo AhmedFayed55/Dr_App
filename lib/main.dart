86783c3ba0a1071158d107466ab53bf493246487
Fayed <anaa71137@gmail.com>
2025-04-25 23:49:02 +0300

    Build project structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
+import 'package:doctor_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DocApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
